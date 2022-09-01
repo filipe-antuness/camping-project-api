5b9e72c62121f244bc4b8e5230bd78d0cbffe744
Filipe Antunes <80362721+filipe-antuness@users.noreply.github.com>
2022-09-01 20:42:19 -0300

    Add files via upload

---
+++ camping-project/src/main/java/com/api/campingproject/CampingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.api.campingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampingProjectApplication.class, args);
+    }
+
+}
---
+++ camping-project/src/test/java/com/api/campingproject/CampingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.campingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
